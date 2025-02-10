e7482540502c7f8ffa97d180665a6279689f2a82
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-10 10:32:18 +0000

    style the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,8 @@ const Navbar = () => {
           <li> Services </li>
           <li> Portfolio </li>
           <li> Contact</li>
-        </ul>
+        </ul> 
+        <div className="nav-connect"> Connect With Me</div>
 
     </div>
 

91b75545f24ed88fd3e8a60f4a8be844dcd24909
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-10 10:22:09 +0000

    add five li tags to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import'./Navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        Navbar
+        <img src={logo} alt="logo" /> 
+        <ul className="nav-menu"> 
+          <li> Home </li> 
+          <li> About Me </li>
+          <li> Services </li>
+          <li> Portfolio </li>
+          <li> Contact</li>
+        </ul>
 
     </div>
 

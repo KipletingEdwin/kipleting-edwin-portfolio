8cdd850d85753370b407dcb0baf3f2b397aaedc6
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-11 17:50:33 +0000

    style hero-action div

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <div className='hero'> 
         <img src={profile_img} alt="profile-image" /> 
-        <h1><span> I'm Kipleting Edwin, </span> frontend developer based in the United Kingdom</h1> 
-        <p>I am a frontend developer from Hatfild,Hertfordshire, United Kingdom with 10 years of </p> 
+        <h1><span> I'm Kipleting Edwin, </span> fullstack developer based in the United Kingdom</h1> 
+        <p>I am a frontend developer based in Hatfield, Hertfordshire, United Kingdom, with experience in JavaScript, React, Ruby on Rails, and Java. I am currently pursuing an MSc in Software Engineering. </p> 
         <div className="hero-action"> 
             <div className="hero-connect"> Connect with me</div> 
             <div className="hero-resume">My resume</div>

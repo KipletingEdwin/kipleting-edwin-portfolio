6197702a6ac7691643012f09b66eef0fe242e9c0
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-11 09:58:35 +0000

    add short description of hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'> 
+        <img src={profile_img} alt="profile-image" /> 
+        <h1>I'm Kipleting Edwin, frontend developer based in the United Kingdom</h1> 
+        <p>I am a frontend developer from Hatfild,Hertfordshire, United Kingdom with 10 years of </p> 
+        <div className="hero-action"> 
+            <div className="hero-connect"> Connect with me</div> 
+            <div className="hero-resume">My resume</div>
+
+
+        </div>
+    </div>
   )
 }
 

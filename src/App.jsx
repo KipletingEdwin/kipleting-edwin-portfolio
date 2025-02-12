c85c762fd2d3887333fa0993fedf39a6eb791524
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-12 10:08:48 +0000

    Add About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
 
 
 const App = () => {
   return (
     <div> 
       <Navbar /> 
-      <Hero />
+      <Hero /> 
+      <About />
 
     </div>
   )

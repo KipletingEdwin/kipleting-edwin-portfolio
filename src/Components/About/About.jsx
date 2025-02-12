bc00d71e4955ed0b2ee4c70af1a0cda68efcb075
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-12 15:48:43 +0000

    add years of experinec

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -24,11 +24,24 @@ const About = () => {
 
           <div className="about-skills"> 
             <div className="about-skill"><p> HTML & CSS </p> <hr style={{width: "50%"}}  /> </div> 
-            <div className="about-skill"><p> React JS </p> <hr style={{width: "50%"}}  /> </div>
-            <div className="about-skill"><p> JavaScript </p> <hr style={{width: "50%"}}  /> </div>
-            <div className="about-skill"><p> Ruby on Rails </p> <hr style={{width: "50%"}}  /> </div>
+            <div className="about-skill"><p> React JS </p> <hr style={{width: "70%"}}  /> </div>
+            <div className="about-skill"><p> JavaScript </p> <hr style={{width: "60%"}}  /> </div>
+            <div className="about-skill"><p> Ruby on Rails </p> <hr style={{width: "75%"}}  /> </div>
             <div className="about-skill"><p> Java </p> <hr style={{width: "50%"}}  /> </div>
           </div>
+        </div>
+        <div className="about-achievements"> 
+          <div className="about-achievement">
+            <h1>10+</h1> 
+            <p>YEARS OF EXPERIENCE</p>
+          </div> 
+          <hr />
+
+          <div className="about-achievement">
+            <h1>90+</h1> 
+            <p>PROJECTS COMPLETED</p>
+          </div> 
+
         </div>
     </div>
   )

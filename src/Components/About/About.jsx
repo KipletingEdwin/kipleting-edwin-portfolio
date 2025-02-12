05dc65c5364a49888207a00f6e06836e5775a6fa
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-12 15:26:15 +0000

    add about-title

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,13 +1,23 @@
 import React from 'react' 
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
+import profile_img from '../../assets/about_profile.svg'
 
 const About = () => {
   return (
     <div className='about'>
         <div className="about-title">
             <h1>About Me</h1> 
-            <img src={theme_pattern} alt="theme" />
+            <img src={theme_pattern} alt="theme" /> 
+        </div> 
+        <div className="about-sections"> 
+          <div className="about-left"> 
+            <img src={profile_img} alt="profile-img" />
+          </div>
+          <div className="about-right">
+            
+          </div>
         </div>
     </div>
   )

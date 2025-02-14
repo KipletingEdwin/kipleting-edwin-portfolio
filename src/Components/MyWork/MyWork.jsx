b8bc9233d98643f734e778376e9846711653c633
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-14 18:50:26 +0000

    add h1 to my work

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './MyWork.css'
+import theme_pattern from '../../assets/theme_pattern.svg';
+import mywork_data from '../../assets/mywork_data'
 
 const MyWork = () => {
   return (
-    <div>MyWork</div>
+    <div className='myWork'> 
+        <div className="mywork-title">
+            <h1>My latest work</h1>
+        </div>
+    </div>
   )
 }
 

722eed401fe0b82319ccba1e1b5a41281707ca38
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-15 11:05:03 +0000

    add html structure for footer-top-left

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import'./Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import footer_logo from '../../assets/footer_logo.svg' 
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
@@ -9,6 +10,13 @@ const Footer = () => {
         <div className="footer-top-left">
           <img src={footer_logo} alt="footer-logo" /> 
           <p> I am a frontend developer from Hatfield,Hertfordshire, United Kingdom</p>
+        </div> 
+        <div className='footer-top-right'> 
+          <div className="footer-email-input">
+            <img src={user_icon} alt="user_icon" /> 
+            <input type="text" placeholder='Enter your email' />
+          </div>
+
         </div>
       </div>
     </div>

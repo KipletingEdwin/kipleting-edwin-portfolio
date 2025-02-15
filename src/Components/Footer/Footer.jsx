8470f28d806904f13052ca0ef412879e82d99eb1
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-15 11:21:38 +0000

    style the footer-top-right

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-top-left">
           <img src={footer_logo} alt="footer-logo" /> 
-          <p> I am a frontend developer from Hatfield,Hertfordshire, United Kingdom</p>
+          <p> I am a frontend developer from Hatfield,Hertfordshire, United Kingdom with 10 years experience in companies like Microsoft,Goggle and Tesla</p>
         </div> 
         <div className='footer-top-right'> 
           <div className="footer-email-input">

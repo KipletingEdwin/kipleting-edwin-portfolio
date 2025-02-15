982fd45e4c6e20b3e692fca2187e7e49a61352a6
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-15 11:01:08 +0000

    add html structure for footer-top-right

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
 
 const Hero = () => {
   return (
-    <div className='hero'> 
-        <img src={profile_img} alt="profile-image" /> 
-        <h1><span> I'm Kipleting Edwin, </span> fullstack developer based in the United Kingdom</h1> 
-        <p>I am a frontend developer based in Hatfield, Hertfordshire, United Kingdom, with experience in JavaScript, React, Ruby on Rails, and Java. I am currently pursuing an MSc in Software Engineering. </p> 
-        <div className="hero-action"> 
-            <div className="hero-connect"> Connect with me</div> 
-            <div className="hero-resume">My resume</div>
-        </div>
+    <div className="hero">
+      <img src={profile_img} alt="profile-image" />
+      <h1>
+        <span> I'm Kipleting Edwin, </span> fullstack developer based in the
+        United Kingdom
+      </h1>
+      <p>
+        I am a frontend developer based in Hatfield, Hertfordshire, United
+        Kingdom, with experience in JavaScript, React, Ruby on Rails, and Java.
+        I am currently pursuing an MSc in Software Engineering.{" "}
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect"> Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

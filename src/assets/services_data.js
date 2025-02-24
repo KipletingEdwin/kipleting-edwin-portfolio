da83eb77cfd6df33ea5841fa677c1feee7ff0364
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-24 15:37:26 +0000

    refacror my projets

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,29 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Web Appplication Development",
+        s_desc:"I build modern, scalable, and user-friendly web applications using technologies like JavaScript, React, and Rails."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Backend Development",
+        s_desc:"I develop robust server-side applications with RESTful APIs, databases, and authentication systems using Ruby on Rails and Node.js."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Database Design & Optimization",
+        s_desc:"I design, manage, and optimize relational and NoSQL databases for efficient data storage and retrieval."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Software Testing & Debugging",
+        s_desc:"I ensure software quality through automated testing, debugging, and performance optimization."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
+        s_name:"Version Control & Collaboration",
+        s_desc:"I use Git, GitHub, and CI/CD pipelines to collaborate effectively in software development projects."
+    }
 ]
  
 export default Services_Data;

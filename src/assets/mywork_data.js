44e415c98af18d589daab867f755783480d50e56
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-04-05 18:23:01 +0100

    make projects responsive

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,5 @@
 import project1_img from '../assets/project_1.png'
-import project2_img from '../assets/project_2.png'
-import project3_img from '../assets/project_3.svg'
+import project3_img from '../assets/project_3.png'
 
 
 const mywork_data = [
@@ -8,11 +7,6 @@ const mywork_data = [
         w_no:1,
         w_name:"Web design",
         w_img:project1_img
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
     },
     {
         w_no:3,
